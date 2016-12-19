520edf50ead2be7f8b515e56a9486bf1a4800ee8
Robin van Baalen <robin@cattlea.com>
2016-12-19 11:45:02 -0400

    Display source of sandbox.php on main page

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
+<?php
+$includePath = '../src/sandbox.php';
+?>
 <!doctype html>
 <html class="no-js" lang="">
 <head>
@@ -9,19 +12,39 @@
 
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/validform.css">
+    <link rel="stylesheet" href="css/monokai-sublime.css">
 
     <script src="js/jquery.min.js"></script>
     <script src="js/validform.js"></script>
     <script src="js/validwizard.js"></script>
+
+    <script src="js/highlight.js"></script>
 </head>
 <body>
 
 <h1>ValidForm Builder Playground</h1>
 <p>This file serves as a playground for ValidForm Builder. Edit the contents of <code>src/sandbox.php</code> to change the form below. Alternatively, you can <a href="examples.php">browse through the form examples</a>.</p>
 
+<h2><?=$includePath?></h2>
+<pre>
+<code>
 <?php
-require_once('../src/sandbox.php');
+//*** Get the source file and show it as a reference for users
+$source = file_get_contents($includePath);
+$source = htmlspecialchars($source);
+
+echo $source;
+?>
+</code>
+</pre>
+<hr>
+<h2>Output</h2>
+<?php
+
+    require_once($includePath);
+
 ?>
 
+<script>hljs.initHighlightingOnLoad();</script>
 </body>
 </html>
